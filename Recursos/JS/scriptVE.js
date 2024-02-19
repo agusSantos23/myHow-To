211a08c646e6ccf5791b8233b6baf82fa0083148
AguSSantos Prieto Atienza <131393036+agusSantos23@users.noreply.github.com>
2024-02-19 09:03:45 +0100

    0.5
    VE terminado

---
+++ Recursos/JS/scriptVE.js
@@ -0,0 +1,23 @@
+function winlinVE(id){
+
+    switch(id){
+        case 1:
+
+        break;
+
+
+        case 2:
+
+        break
+    }
+
+    document.getElementById("VE").style.display = "flex";
+}
+
+
+function cerrarVE(){
+
+    document.getElementById("VE").style.display = "none";
+
+}
+
