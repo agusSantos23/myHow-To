98b7a3a55f7ee0dcc0d63c52addfdaa9bc208787
AguSSantos Prieto Atienza <131393036+agusSantos23@users.noreply.github.com>
2024-02-20 13:07:25 +0100

    0.7
    Descargas de MV VE

--- Recursos/JS/scriptVE.js
+++ Recursos/JS/scriptVE.js
@@ -2,22 +2,25 @@ function winlinVE(id){
 
     switch(id){
         case 1:
+        document.getElementById("VE1").style.display = "flex";
 
         break;
 
 
         case 2:
+        document.getElementById("VE2").style.display = "flex";
 
         break
     }
 
-    document.getElementById("VE").style.display = "flex";
 }
 
 
 function cerrarVE(){
 
-    document.getElementById("VE").style.display = "none";
+    document.getElementById("VE1").style.display = "none";
+    document.getElementById("VE2").style.display = "none";
+
 
 }
 
